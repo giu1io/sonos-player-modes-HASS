d1d9ef6cd7016dbf56f229c46305ede930fe9bb5
Giulio Montagner <me@giuliomontagner.com>
2019-09-10 09:28:57 +0200

    Updated to use already discovered sonos devices and their entity ids.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Sonos Player Modes
 ## Custom Component For Home Assistant
 
-This component creates a service `sonos_player_modes.set_mode` that lets you change playback modes on any one of your Sonos speakers.
+This component creates a service `sonos_player_modes.set_mode` that lets you change playback modes on any one of your Sonos speakers. Requires the `sonos` component to be already setup.
 
 ## Service Parameters
-- `device_name`: name of the device in the Sonos app
+- `entity_id`: entity id of the speaker  (eg. `media_player.bedroom`)
 - `mode`: playback mode. Supported modes: `NORMAL`, `REPEAT_ALL`, `SHUFFLE`, `SHUFFLE_NOREPEAT`
--- custom_components/sonos_player_modes/__init__.py
+++ custom_components/sonos_player_modes/__init__.py
@@ -1,23 +1,54 @@
-from soco.discovery import by_name
+""" Sonos Player Modes """
 import logging
+from homeassistant.components import sonos
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = 'sonos_player_modes'
+DOMAIN = "sonos_player_modes"
+
+DEPENDENCIES = ["sonos"]
+
 
 def setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
 
-    def set_mode(call):
-        """Handle the service call."""
-        mode = call.data.get('mode', 'NORMAL')
-        device_name = call.data.get('device_name', 'Bedroom')
+    sonos_player_data = SonosPlayerData(hass)
+    hass.services.register(DOMAIN, "set_mode", sonos_player_data.set_mode)
 
-        device = by_name(device_name)
+    _LOGGER.debug("Started Sonos Player Modes")
 
-        device.play_mode = mode
-
-    hass.services.register(DOMAIN, 'set_mode', set_mode)
-
-    # Return boolean to indicate that initialization was successfully.
     return True
+
+
+class SonosPlayerData(object):
+    """Controls Sonos Speakers discovered by the Sonos Integration."""
+
+    def __init__(self, hass):
+        """Initialize the data object."""
+        self.hass = hass
+
+    def get_soco_from_entity_id(self, entity_id):
+        """Returns the SoCo object of the entity that corresponds to the given entity_id"""
+        data_sonos = self.hass.data[sonos.media_player.DATA_SONOS]
+
+        for entity in data_sonos.entities:
+            if entity.entity_id == entity_id:
+                break
+        else:
+            entity = None
+
+        if entity is None:
+            _LOGGER.error("No entity found for entity_id: %s", entity_id)
+            return None
+
+        return entity.soco
+
+    def set_mode(self, call):
+        """Handle the service call."""
+        mode = call.data.get("mode", "NORMAL")
+        entity_id = call.data.get("entity_id", "media_player.bedroom")
+
+        device = self.get_soco_from_entity_id(entity_id)
+
+        if device is not None:
+            device.play_mode = mode
